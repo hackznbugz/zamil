46ea469f390354642ff6e2fc0b7c3f1dba3a1f23
Akbar Ali <51843650+hackznbugz@users.noreply.github.com>
2021-08-03 20:10:36 +0530

    Update main.js

--- contact/js/main.js
+++ contact/js/main.js
@@ -1,3 +1,13 @@
 $('.explore').click(function(){
   window.scroll(0,findPos(document.getElementsByClassName("box")[0]));
 })
+
+function findPos(obj) {
+    var curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+    return [curtop];
+    }
+}
