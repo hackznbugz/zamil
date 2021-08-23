2348df33f1c693682e0376b8e4167f3f431ba5c8
Akbar Ali <51843650+hackznbugz@users.noreply.github.com>
2021-08-23 23:03:37 +0530

    Add files via upload

---
+++ about/main.js
@@ -0,0 +1,17 @@
+isPlaying = false;
+
+function PlayOrPause(){
+    if(!isPlaying){
+        $('#video')[0].play();
+        $('.video').addClass('playing');
+        $('.video .overlay img').attr("src","../img/pause-button.png");
+        $('.video .overlay p').html("Click here to pause video");
+        isPlaying = true;
+    }else{
+        $('#video')[0].pause();
+        $('.video').removeClass('playing');
+        $('.video .overlay img').attr("src","../img/play-button.png");
+        $('.video .overlay p').html("Click here to play video");
+        isPlaying = false;
+    }
+}
